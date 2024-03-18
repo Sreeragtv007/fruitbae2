997f69e836ed8275adf3da45368727f74da2da3c
sreerag <sreeragtv007@gmail.com>
2024-03-18 11:15:10 +0530

    home page fixed

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    desc = models.TextField(blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+    image = models.ImageField(
+        upload_to='images', default='image not available')
+
+    thumbnails = models.ImageField(upload_to='thumbnail', blank=True)
+
+    categ = models.ForeignKey(
+        'category', on_delete=models.CASCADE, blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+  
+        
+        
+        
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request,'index.html')
+    products=Product.objects.all()
+    context={'products':products}
+    return render(request,'index.html',context)
