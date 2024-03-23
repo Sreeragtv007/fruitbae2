dd6bd594491108e77fee2af000565c849679706d
sreerag <sreeragtv007@gmail.com>
2024-03-23 19:56:22 +0530

    pagination added

--- app/views.py
+++ app/views.py
@@ -2,18 +2,32 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 # Create your views here.
 
 
 @login_required(login_url='login')
 def index(request, **kwargs):
+    
     user = User.objects.all().count()
     category = Category.objects.all()
     products = Product.objects.all()
     total_products = products.count()
 
-    context = {'products': products, 'category': category,
-               'user': user, 'total_products': total_products}
+    p = Paginator(products,2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)  
+    except PageNotAnInteger:
+        page_obj = p.page(1)
+    except EmptyPage:
+        
+        page_obj = p.page(p.num_pages)
+
+
+    context = { 'category': category,
+               'user': user, 'total_products': total_products,'page_obj': page_obj, }
     return render(request, 'products.html', context)
 
 
