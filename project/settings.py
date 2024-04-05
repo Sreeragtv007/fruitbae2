11464fd227b9f5a4daa90f7c442c7bb90d1895d9
sreerag <sreeragtv007@gmail.com>
2024-04-05 10:43:20 +0530

    css added to internally

--- project/settings.py
+++ project/settings.py
@@ -128,13 +128,13 @@ MEDIA_URL = '/media/'
 
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
     
-]
+# ]
 
 
 
